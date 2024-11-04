dbef9a381377d6b10d2e47f90bce6455bdd52d13
riteshpersonal <rudragond2020@gmail.com>
2024-11-04 19:51:20 +0530

    Your commit message here

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from rich.table import Table
 load_dotenv(find_dotenv())
 console = Console()
 
-# Declaring ENV Variables 
+# Declare 
 
 MYSQL_HOST: Final = getenv('MYSQL_HOST')
 MYSQL_PORT: Final = getenv('MYSQL_PORT')
